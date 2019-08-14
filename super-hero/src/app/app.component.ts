f355f37d4358ae32ae1306467ba1e2a05bf1bac4
Faraz Ahmad <canny.faraz@gmail.com>
2019-08-14 15:35:43 +0530

    Calling heroes with code

--- super-hero/src/app/app.component.ts
+++ super-hero/src/app/app.component.ts
@@ -12,6 +12,8 @@ export class AppComponent implements OnInit {
   title = 'super-hero';
   superHeroForm: FormGroup;
   codeValue: any = '';
+  superHero: any;
+  errorMesage: any;
   constructor(private _backedService: BackendServiceService, private _formBuilder: FormBuilder){
     this.superHeroForm = this._formBuilder.group({
       hero_input: ['']
@@ -19,10 +21,7 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit(){
-    this._backedService.callHero('0 8737626').subscribe(data=>{
-      console.log("Data ==> ",data);
-      
-    })
+    
   }
 
   setValue(value){
@@ -31,5 +30,31 @@ export class AppComponent implements OnInit {
       value = ' '
     this.codeValue += value;
     this.superHeroForm.controls['hero_input'].setValue(this.codeValue)
+  }
+
+  callHero(){
+    this.superHero = null
+    if(this.codeValue){
+      this._backedService.callHero(this.codeValue).subscribe(data=>{
+        if(data.code == 0){
+          this.errorMesage = data.message;
+        }
+        if(data.hero){
+          this.errorMesage = null
+          this.superHero = data.hero;
+        }
+      },
+      err=>{
+        console.log(err.error);
+        this.errorMesage = err.error.hero
+      })
+    }else{
+      this.errorMesage = 'Please Enter the Code to call the Super Hero'
+    }
+  }
+
+  clearField(){
+    this.codeValue = '';
+    this.superHeroForm.controls['hero_input'].reset()
   }
 }
